6cbf1c3e5249910c53cbaebab25dd9de3a7cc909
csh3424 <csh3424@icloud.com>
2025-09-03 15:10:31 +0900

    테스트 코드

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+# list_audio.py
 import pyaudio
-p = pyaudio.PyAudio()
-for i in range(p.get_device_count()):
-    info = p.get_device_info_by_index(i)
-    print(f"Index {i}: {info['name']} (in:{info['maxInputChannels']}, out:{info['maxOutputChannels']})")
+pa = pyaudio.PyAudio()
+for i in range(pa.get_device_count()):
+    info = pa.get_device_info_by_index(i)
+    print(i, info["name"], "| host:", info["hostApi"], "| rate:", int(info["defaultSampleRate"]))
+pa.terminate()
